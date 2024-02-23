e960a94eff8b5313c02a8fdb33674b1a23330f7e
hd-qxc-0411 <331125363@qq.com>
2024-02-23 13:48:15 +0800

    - impl sample inject

---
+++ injector.go
@@ -0,0 +1,134 @@
+// Package ginject
+//
+//	@Description
+//	@Author  hd_0411_qxc  2024/2/23 13:13
+//	@Update  hd_0411_qxc  2024/2/23 13:13
+package ginject
+
+import (
+	"context"
+	"reflect"
+
+	"github.com/gogf/gf/v2/container/gvar"
+	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/os/gstructs"
+	"github.com/gogf/gf/v2/util/gmeta"
+)
+
+type DataSource interface {
+	MustGet(ctx context.Context, pattern string, def ...interface{}) *gvar.Var
+}
+
+type injector struct {
+	prefix string
+	data   DataSource
+}
+
+type Injector interface {
+	Apply(d interface{}) error
+	Sub(prefix string) Injector
+}
+
+func New(data DataSource) Injector {
+	return &injector{
+		prefix: "",
+		data:   data,
+	}
+}
+
+func NewWithPrefix(data DataSource, prefix string) Injector {
+	return &injector{
+		prefix: prefix,
+		data:   data,
+	}
+}
+
+func joinPath(prefix, sub string) string {
+	if prefix == "" {
+		return sub
+	}
+	if sub == "" {
+		return prefix
+	}
+	return prefix + "." + sub
+}
+
+func writeableReflectValue(d interface{}, f gstructs.Field) reflect.Value {
+	if f.IsExported() {
+		return f.Value
+	}
+	return GetPtrUnexportFiled(d, f.Name())
+}
+func (inj *injector) Sub(prefix string) Injector {
+	return &injector{
+		prefix: joinPath(inj.prefix, prefix),
+		data:   inj.data,
+	}
+}
+
+func (inj *injector) Apply(d interface{}) error {
+	defer func() {
+		if r := recover(); r != nil {
+			println(r)
+			return
+		}
+	}()
+
+	v := reflect.ValueOf(d)
+	if v.Kind() == reflect.Ptr || v.Kind() == reflect.Interface {
+		v = v.Elem()
+	}
+	if v.Kind() != reflect.Struct {
+		return gerror.New("need struct")
+	}
+	return inj.doApplyStruct(context.TODO(), d, inj.prefix)
+}
+
+func (inj *injector) doApplyStruct(ctx context.Context, d interface{}, prefix string) error {
+	// reset prefix if the struct has gmeta.Meta
+	prefix = joinPath(prefix, gmeta.Get(d, "prefix").String())
+	allFields, _ := gstructs.Fields(gstructs.FieldsInput{
+		Pointer:         d,
+		RecursiveOption: 0,
+	})
+	for _, field := range allFields {
+		var (
+			bind string
+			def  string
+		)
+		if !field.Value.CanAddr() {
+			continue
+		}
+		// if the field is a struct, do recursive injection and skip the next steps
+		if field.Kind() == reflect.Struct {
+			_ = inj.doApplyStruct(ctx, GetPtrUnexportFiled(d, field.Name()).Addr().Interface(), prefix)
+			continue
+		}
+		if bind = field.Tag("inject"); bind == "" {
+			// if the field has no bind tag, it does not need to be injected
+			continue
+		}
+		// get the value from the data source
+		bindPath := joinPath(prefix, bind)
+		def, _ = field.TagLookup("def")
+		// get the reflect.Value of the field which is writeable
+		rv := GetWriteAbleReflectValue(d, field)
+		switch field.Kind() {
+		case reflect.String:
+			val := inj.data.MustGet(ctx, bindPath, def).String()
+			rv.SetString(val)
+		case reflect.Bool:
+			val := inj.data.MustGet(ctx, bindPath, def).Bool()
+			rv.SetBool(val)
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			val := inj.data.MustGet(ctx, bindPath, def).Int64()
+			rv.SetInt(val)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			val := inj.data.MustGet(ctx, bindPath, def).Uint64()
+			rv.SetUint(val)
+		default:
+			continue
+		}
+	}
+	return nil
+}
---
+++ z_test.go
@@ -0,0 +1,42 @@
+// Package ginject
+//
+//	@Description
+//	@Author  hd_0411_qxc  2024/2/23 13:40
+//	@Update  hd_0411_qxc  2024/2/23 13:40
+package ginject
+
+import (
+	"testing"
+
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/util/gmeta"
+)
+
+type Object struct {
+	gmeta.Meta `prefix:"app"`
+	appName    string `inject:"name" def:"app_default"`
+	version    string `inject:"version" def:"1.0.0"`
+	mqtt       struct {
+		gmeta.Meta `prefix:"mqtt"`
+		broker     string `inject:"broker" def:"tcp://10.147.198.110:1883"`
+		clientId   string `inject:"clientId" def:""`
+		username   string `inject:"username" def:""`
+		password   string `inject:"password" def:""`
+	}
+	number struct {
+		number1 int   `inject:"number" def:"1"`
+		number2 int8  `inject:"number" def:"2"`
+		number3 int16 `inject:"number" def:"3"`
+		number4 int32 `inject:"number" def:"4"`
+		number5 int64 `inject:"number" def:"5"`
+	}
+}
+
+func TestInject(t *testing.T) {
+	obj := &Object{}
+	inj := New(g.Cfg())
+	if err := inj.Apply(obj); err != nil {
+		t.Error("injector failed", err)
+	}
+	g.Dump(obj)
+}
